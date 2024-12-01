92e78c4e466cf69fcdbf4ffe47758936bed34d2d
Adam Hutchison <adam.hutchison@hey.com>
2024-12-01 15:05:00 -0700

    Return 404 HEAD response by default
    Update the Allow middleware to return a 404 HEAD response (with empty
    headers and body), and add config options for :content, :content_type,
    and :status. If :content is nil (the default), no headers will be
    returned. Otherwise, a content-type header will be added. If
    :content_type is nil (the default), "text/plain" will be used.
    
    With this change, :message is no longer needed and has been removed (use
    :content instead).

--- test/strong_routes/config_test.rb
+++ test/strong_routes/config_test.rb
@@ -8,6 +8,14 @@ describe StrongRoutes::Config do
       _(subject.respond_to?(:allowed_routes)).must_equal true
     end
 
+    it "supports :content" do
+      _(subject.respond_to?(:content)).must_equal true
+    end
+
+    it "supports :content_type" do
+      _(subject.respond_to?(:content_type)).must_equal true
+    end
+
     it "supports :enabled" do
       _(subject.respond_to?(:enabled)).must_equal true
     end
@@ -27,11 +35,9 @@ describe StrongRoutes::Config do
     it "supports :insert_before?" do
       _(subject.respond_to?(:insert_before?)).must_equal true
     end
-  end
 
-  describe "#enabled" do
-    it "is enabled by default" do
-      _(subject).must_be :enabled
+    it "supports :status=" do
+      _(subject.respond_to?(:status=)).must_equal true
     end
   end
 
@@ -39,6 +45,46 @@ describe StrongRoutes::Config do
     it "maps allowed routes to matchers" do
       subject.allowed_routes = ["/users"]
       _(subject.route_matchers).must_equal [StrongRoutes::RouteMatcher.new("/users")]
+    end
+  end
+
+  describe "#enabled?" do
+    it "is true" do
+      _(subject).must_be :enabled
+    end
+  end
+
+  describe "#headers" do
+    it "is an empty hash" do
+      _(subject.headers).must_equal({})
+    end
+
+    context "with content_type: 'text/plain'" do
+      it "is an empty hash" do
+        subject.content_type = "text/plain"
+        _(subject.headers).must_equal({})
+      end
+    end
+
+    context "with content: 'Nope' and content_type: nil" do
+      it "contains Content-Type headers" do
+        subject.content = "Nope"
+        _(subject.headers).must_equal({"content-type" => "text/plain"})
+      end
+    end
+
+    context "with content: 'Nope' and content_type: 'text/plain'" do
+      it "contains Content-Type headers" do
+        subject.content = "Nope"
+        subject.content_type = "text/plain"
+        _(subject.headers).must_equal({"content-type" => "text/plain"})
+      end
+    end
+  end
+
+  describe "#status" do
+    it "is 404" do
+      _(subject.status).must_equal 404
     end
   end
 end
